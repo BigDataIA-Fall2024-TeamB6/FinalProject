5172144e140e7d5513c13ca0dbfc544ff1ad7838
kumarsandeep567 <kumar.sandee@northeastern.edu>
2024-11-22 15:49:37 -0500

    Added POC for connecting to Personal Outlook account

---
+++ Graph_API_Personal_Account/app_config.py
@@ -0,0 +1,39 @@
+import os
+
+# Application (client) ID of app registration
+CLIENT_ID = os.getenv("CLIENT_ID")
+
+# Application's generated client secret: never check this into source control!
+CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+
+# AUTHORITY = "https://login.microsoftonline.com/common"  # For multi-tenant app
+AUTHORITY = f"https://login.microsoftonline.com/{os.getenv('TENANT_ID', 'common')}"
+
+REDIRECT_PATH = "/getAToken"  # Used for forming an absolute URL to your redirect URI.
+# The absolute URL must match the redirect URI you set
+# in the app's registration in the Azure portal.
+
+# You can find more Microsoft Graph API endpoints from Graph Explorer
+# https://developer.microsoft.com/en-us/graph/graph-explorer
+# ENDPOINT = 'https://graph.microsoft.com/v1.0/users'  # This resource requires no admin consent
+ENDPOINT = 'https://graph.microsoft.com/v1.0/me/messages'
+
+# You can find the proper permission names from this document
+# https://docs.microsoft.com/en-us/graph/permissions-reference
+# SCOPE = ["User.ReadBasic.All"]
+SCOPE = [
+    "User.ReadBasic.All", 
+    "User.read", 
+    "Files.Readwrite.All", 
+    "Sites.Readwrite.All", 
+    "Mail.ReadWrite", 
+    "Mail.ReadBasic", 
+    "Mail.ReadWrite", 
+    "MailboxSettings.Read",
+    "Mail.Send"
+]
+
+# Tells the Flask-session extension to store sessions in the filesystem
+SESSION_TYPE = "filesystem"
+# Using the file system will not work in most production systems,
+# it's better to use a database-backed session store instead.
